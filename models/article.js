dc7ef5a0059ee09a4f493b82d1972a107d0969f0
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-02-19 15:31:38 -0600

    Remove test

--- models/article.js
+++ models/article.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var articleSchema = require('../db/schema');
 var Article = mongoose.model('Article', articleSchema);
 
-Article.create({
-    title: 'First',
-    auther: 'Some person',
-    link: 'google.com'
-}, function(err, small) {
-    if (err) return handleError(err);
-});
+// Article.create({
+//     title: 'First',
+//     auther: 'Some person',
+//     link: 'google.com'
+// }, function(err, small) {
+//     if (err) return handleError(err);
+// });
 
 // mongoose.connect('mongodb://localhost/test');
 mongoose.connect('mongodb://heroku_w84ktq71:ipefkl8ksohi858fviu2j7o7ib@ds019471.mlab.com:19471/heroku_w84ktq71');
